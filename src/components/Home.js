6179e3231c61aa68c31f05e8c0483dac791b1fc2
github username <bballwiz4@gmail.com>
2020-08-28 21:12:21 -0700

    Started to add the projects aspect to the home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import projects from '../data.js';
 const Home = (props) => {
     return (
         <div className="home">
@@ -8,14 +9,25 @@ const Home = (props) => {
             <div className="banner-bio">
                 <div className="row">
                     <div className="four columns">
-                        <img id="portrait" src="portrait.jpeg" alt="Jake Garcia" />
+                        <img id="portrait" src="imgs/portrait.jpeg" alt="Jake Garcia" />
                     </div>
                     <div className="eight columns">
-                        <p>
-                            Hi my name is Jake and to keep it short so you'll check out my about page, I have created this website to showcase my projects and create an online presence.
+                        <p id="quick-bio">
+                            Hi my name is Jake and i'll keep it short so you'll check out my about page. I have created this website to showcase my projects and create an online presence.
                         </p>
                     </div>
                 </div>
+            </div>
+            <div className="row" id="home-projects-container">
+                {projects.map(project => {
+                    return (
+                        <div className="six columns">
+                            <h3>{project.title}</h3>
+                            <p>{project.short}</p>
+                            
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
---
+++ src/data.js
@@ -0,0 +1,21 @@
+
+
+const projects = [
+    {
+        id: 1,
+        title: "COVID-19 Numbers",
+        imgs: ["imgs/COVID-19_Numbers/USTotalNumbers.png", "imgs/COVID-19_Numbers/TopSection.png"],
+        short: "A COVID-19 Web App that displays the numbers as a total for the United States. As well as has a picker to view individual states numbers.",
+    },
+    {
+        id: 2,
+        title: "InspoQuotes",
+        imgs: ["imgs/InspoQuotes/Favorites.PNG", "imgs/InspoQuotes/Home.PNG", "imgs/InspoQuotes/Quote.PNG", "imgs/InspoQuotes/Search.PNG", "imgs/InspoQuotes/SetTime.PNG"],
+        short: "iOS Inspirational Quote App. This app has over 1600 quotes that can be found randomly or by searching for keywords.",
+    },
+    {
+
+    }
+]
+
+export default projects;
