bc8d24addff25c6b82332364a335053414c2ba34
github username <bballwiz4@gmail.com>
2020-09-15 22:36:44 -0700

    contact info added with mailto aability implemented that prefills in my email.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,26 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="container">
-            <h1>Contact Info</h1>
+            <h1 className="contatc-title">Contact Info</h1>
+            <div>
+                <table className="contact-table u-full-width">
+                    <tr>
+                        <th>Type</th>
+                        <th>Address</th>
+                        <th>Specifics</th>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><a href="mailto:jgarciadev4444@gmail.com">jgarciadev4444@gmail.com</a></td>
+                        <td>When sending an email add a subject regarding the main reason for the email.</td>
+                    </tr>
+                    <tr>
+                        <td>Phone</td>
+                        <td>(760)-892-4561</td>
+                        <td>Please leave a voicemail and i will return your call as soon as possible. Texts are also fine.</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 }
