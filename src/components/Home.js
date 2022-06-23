8ed33283852e46d2c07ab0a7544cbb6be64e0190
Jake Garcia <bballwiz4@gmail.com>
2022-06-22 18:52:32 -0700

    Home page has basic verbage there now.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import { projects } from '../data.js';
+import React from 'react';
 import '../styles/Home.css';
-import ProjectOverview from './ProjectOverview/index.js';
 import OpaqueContainer from '../shared/OpaqueContainer/index.js';
-
-const {web, mobile} = projects;
-
-
-
-
+import { HiDesktopComputer, HiDeviceMobile } from 'react-icons/hi';
 
 const Home = (props) => {
-
-    const [opacityVal, setOpacityVal] = useState(0);
 
     return (
         <OpaqueContainer>
             <div className="home">
-                <div className="banner-bio">
-                    <div className="row">
-                        <div className="twelve columns">
-                            <p id="quick-bio">
-                                Welcome to my portfolio site! I am a full software developer. I build web and mobile applications because they often go hand in hand together. Here I am trying to showcase my projects that I have completed as well as showcase my skills growing.
-                            </p>
+                <div className="row welcome-row">
+                    <h1 className="welcome-text">Welcome</h1>
+                </div>
+                <div className="row develop-row">
+                    <h2 className="develop-text">I Develop</h2>
+                </div>
+                <div className="container web-mobile-container">
+                    <div className="five columns web-columns">
+                        <div className="row">
+                            <h3>Web</h3>
+                        </div>
+                        <div className="row">
+                            <HiDesktopComputer className="app-icon" size={116} color="#000" />
                         </div>
                     </div>
+                    <div className="two columns and-columns">
+                        <h3><strong>&</strong></h3>
+                    </div>
+                    <div className="five columns mobile-columns">
+                    <div className="row">
+                            <h3>Mobile</h3>
+                        </div>
+                        <div className="row">
+                            <HiDeviceMobile className="app-icon" size={116} color="#000" />
+                        </div>
+                    </div>
+                    <div className="twelve columns">
+                        <h3>Applications</h3>
+                    </div>
+                    <div className="twelve columns">
+                        <p className="explantion-text">With code written from scratch by yours truly.</p>
+                    </div>
                 </div>
-                {/* <h2 id="projects-title">Projects Overview</h2>
-                <div className="row" id="home-projects-container">
-                    {web.map(project => {
-                        return (
-                            <ProjectOverview key={`web-${project.id}`} project={project} />
-                        )
-                    })}
-                    {mobile.map(project => {
-                        return (
-                            <ProjectOverview key={`mobile-${project.id}`} project={project} />
-                        )
-                    })}
-                </div> */}
+                <div className="container explanation-container">
+                    <h3>I don't use ...</h3>
+                    <ul className="donts-list">
+                        <li>A CMS (content management system)</li>
+                        <li>Click and drag software</li>
+                    </ul>
+                </div>
             </div>
         </OpaqueContainer>
     )
