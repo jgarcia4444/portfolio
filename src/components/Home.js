d9aaa395f9cfb58ef58884aae3448fbde3950bb2
Jake Garcia <bballwiz4@gmail.com>
2022-06-23 10:26:53 -0700

    Created an outbound link component to wrap the languages and frameworks I use to link out to their definitions.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import '../styles/Home.css';
 import OpaqueContainer from '../shared/OpaqueContainer/index.js';
 import { HiDesktopComputer, HiDeviceMobile } from 'react-icons/hi';
+import OutboundLink from '../shared/OutboundLink';
 
 const Home = (props) => {
 
     return (
         <OpaqueContainer>
-            <div className="row home">
+            <div className="container-fluid home">
                 <div className="row welcome-row">
                     <h1 className="welcome-text">Welcome</h1>
                 </div>
@@ -20,25 +21,51 @@ const Home = (props) => {
                             <h3>Web</h3>
                         </div>
                         <div className="row">
-                            <HiDesktopComputer className="app-icon" size={116} color="#000" />
+                            <HiDesktopComputer className="app-icon" size={124} color="#000" />
                         </div>
                     </div>
                     <div className="two columns and-columns">
                         <h3><strong>&</strong></h3>
                     </div>
                     <div className="five columns mobile-columns">
-                    <div className="row">
+                        <div className="row">
                             <h3>Mobile</h3>
                         </div>
                         <div className="row">
-                            <HiDeviceMobile className="app-icon" size={116} color="#000" />
+                            <HiDeviceMobile className="app-icon" size={124} color="#000" />
                         </div>
                     </div>
                     <div className="twelve columns">
                         <h3>Applications</h3>
                     </div>
-                    <div className="twelve columns">
-                        <p className="explantion-text">With code written from scratch.</p>
+                </div>
+                <div className="container">
+                    <p className="explantion-text">With code written from scratch.</p>
+                    <div className="using-list-container">
+                        <div className="row using-title-row">
+                            <div className="twelve columns">
+                                <h4 className="using-title">Using:</h4>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="six columns left-using-columns">
+                                <ul>
+                                    
+                                    <li><OutboundLink content="HTML" to="https://www.w3schools.com/html/" /></li>
+                                    <li><OutboundLink content="CSS" to="https://developer.mozilla.org/en-US/docs/Web/CSS" /></li>
+                                    <li><OutboundLink content="Javascript" to="https://developer.mozilla.org/en-US/docs/Web/JavaScript" /></li>
+                                    <li><OutboundLink content="Ruby" to="https://www.geeksforgeeks.org/ruby-programming-language/"/></li>
+                                </ul>
+                            </div>
+                            <div className="right-using-columns">
+                                <ul>
+                                    <li><OutboundLink content="Bootstrap (CSS framework)" to="https://www.toptal.com/front-end/what-is-bootstrap-a-short-tutorial-on-the-what-why-and-how" /></li>
+                                    <li><OutboundLink content="React (Javascript web framework)" to="https://reactjs.org/" /></li>
+                                    <li><OutboundLink content="React Native (Javascript mobile framework)" to="https://reactnative.dev/" /></li>
+                                    <li><OutboundLink content="Ruby on Rails (Ruby backend framework)" to="https://rubyonrails.org/" /></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="container explanation-container">
