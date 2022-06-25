7db97d4fb1db204f3d003894687bbe71f3884f22
Jake Garcia <bballwiz4@gmail.com>
2022-06-24 23:10:25 -0700

    Created the rendering method using the custom hook useWindowDimensions to render a different nav menu when the screen size gets to 600 pixels and below. Then added a state variable that shows and dismisses the mobile menu. Added styling to the mobile menu to make it appear nice.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import '../styles/NavBar.css';
+import useWindowDimensions from '../CustomHooks/useWindowDimensions';
+import {HiMenu} from 'react-icons/hi';
 
 const NavBar = (props) => {
+
+    const {height, width} = useWindowDimensions();
+    const [showMobileNavLinks, setShowMobileNavLinks] = useState(false);
+
+    const computerViewNavLinks =  (
+        <div className="nav-items-container">
+            <div className="nav-item">
+                <Link className="nav-links" to="/">Home</Link>
+            </div>
+            <div className="nav-item">
+                <Link className="nav-links" to="/about">About</Link>
+            </div>
+            <div className="nav-item">
+                <Link className="nav-links" to="/projects">Projects</Link>
+            </div>
+        </div>
+    );
+
+    const mobileViewNavLinks = (
+        <div className="mobile-nav-items-container">
+            <div onClick={() => setShowMobileNavLinks(!showMobileNavLinks)} className="mobile-menu-button">
+                <HiMenu color={'white'} size={36} />
+            </div>
+            {showMobileNavLinks === true &&
+                <div className="nav-links-modal">
+                    <div className="mobile-nav-link-row">
+                        <Link onClick={() => setShowMobileNavLinks(false)} to="/" className="mobile-nav-link">Home</Link>
+                    </div>
+                    <div className="mobile-nav-link-row">
+                        <Link onClick={() => setShowMobileNavLinks(false)} to="/about" className="mobile-nav-link">About</Link>
+                    </div>
+                    <div className="mobile-nav-link-row">
+                        <Link onClick={() => setShowMobileNavLinks(false)} to="/projects" className="mobile-nav-link">Projects</Link>
+                    </div>
+                    <div className="mobile-nav-link-row">
+                        <Link onClick={() => setShowMobileNavLinks(false)} to="/contact" className="mobile-nav-link">Contact</Link>
+                    </div>
+                </div>
+            }
+        </div>
+    );
+
+    const renderNavLinks = () => {
+        if (width < 601) {
+            return mobileViewNavLinks;
+        } else {
+            return computerViewNavLinks    
+        }
+    }
+
     return (
         <div className="navbar">
             <div className="nav-title-container">
@@ -12,17 +64,7 @@ const NavBar = (props) => {
                     </Link>
                 </h5>
             </div>
-            <div className="nav-items-container">
-                <div className="nav-item">
-                    <Link className="nav-links" to="/">Home</Link>
-                </div>
-                <div className="nav-item">
-                    <Link className="nav-links" to="/about">About</Link>
-                </div>
-                <div className="nav-item">
-                    <Link className="nav-links" to="/projects">Projects</Link>
-                </div>
-            </div>
+            {renderNavLinks()}
         </div>
     )
 }
