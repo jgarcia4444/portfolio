accff13d6fd1bfe71a32ef300af1c58d81434d8c
Jake Garcia <bballwiz4@gmail.com>
2022-06-26 15:47:39 -0700

    wrapped the contact info as well as the icon that relates to the info with an anchor tag, so that the action that takes place when the contact information is tapped also happens when the corresponding icon is tapped.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import OpaqueContainer from '../shared/OpaqueContainer';
 import '../styles/Contact.css';
+import { HiMail, HiPhone } from 'react-icons/hi';
 
 const Contact = () => {
     return (
         <OpaqueContainer>
             <div className="container contact-page-container">
                 <h1 className="contact-title">Contact Info</h1>
-                <div>
-                    <table className="contact-table u-full-width">
-                        <tr>
-                            <th>Type</th>
-                            <th>Address</th>
-                            <th>Details</th>
-                        </tr>
-                        <tr>
-                            <td><strong>Email</strong></td>
-                            <td><a className="contact-anchor" href="mailto:jgarciadev4444@gmail.com">jgarciadev4444@gmail.com</a></td>
-                            <td className="contact-details">When sending an email add a subject regarding the main reason for the email.</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Phone</strong></td>
-                            <td><a className="contact-anchor" href="tel:17608924561">(760)-892-4561</a></td>
-                            <td className="contact-details">I will try my best to answer all calls. Texts are also fine.</td>
-                        </tr>
-                    </table>
+                <div className="contact-table">
+                    <div className="contact-info-buttons">
+                        <a href="mailto:jgarciadev4444@gmail.com" className="contact-button email-button">
+                            <HiMail size={32} color='white' />
+                            <div className="contact-anchor-info" >jgarciadev4444@gmail.com</div>
+                        </a>
+                        <a href="tel:17608924561" className="contact-button phone-button">
+                            <HiPhone size={32} color='white' />
+                            <div className="contac-anchor-info">(760)-892-4561</div>
+                        </a>
+                    </div>
+                </div>
+                <div className="information-container">
+                    <h3>Contact Details</h3>
+                    <ul>
+                        <li><strong>Email:</strong> In the subject add a short phrase of what the email consists of.</li>
+                        <li><strong>Phone:</strong> I will respond to phone calls as well as text messages. Leave a voice mail if I do not answer and I will get back to you as soon as possible.</li>
+                    </ul>
                 </div>
             </div>
         </OpaqueContainer>
